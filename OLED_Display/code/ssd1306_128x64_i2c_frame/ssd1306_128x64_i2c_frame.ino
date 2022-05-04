c14d8a427f534f67a2ad317902b8a8b1a5f51c6e
soboringqqq <wangchuxiao@hotmail.com>
2022-05-04 16:48:32 +0800

    clearn ssd1306_128x64_i2c_frame.ino

--- OLED_Display/code/ssd1306_128x64_i2c_frame/ssd1306_128x64_i2c_frame.ino
+++ OLED_Display/code/ssd1306_128x64_i2c_frame/ssd1306_128x64_i2c_frame.ino
@@ -18,7 +18,6 @@
  included in any redistribution.
  **************************************************************************/
 
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
